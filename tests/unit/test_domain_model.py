70afcbf6cfd16a36706db2a95e14cdb861e55555
eugenelim17 <62620894+eugenelim17@users.noreply.github.com>
2020-10-09 21:40:16 +1300

    Update test_domain_model.py

--- tests/unit/test_domain_model.py
+++ tests/unit/test_domain_model.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from movie.domain.model import Movie, Genre, User, Review, add_review
+from movie.domain.model import Movie, Genre, User, Review, add_review, WatchList
 
 import pytest
 
@@ -80,6 +80,43 @@ def test_make_review_establishes_relationships(movie, user):
     assert review.movie is movie
 
 
+#### WATCHLIST FUNCTION TEST ####
+
+watchlist = WatchList()
+class MyTestCase():
+    @pytest.fixture
+    def watchlist(self):
+        return WatchList()
+
+    def test_no_first_movie_in_watchlist(self):
+        watchlist.add_movie(Movie("Moana", 2016))
+        assert watchlist.first_movie_in_watchlist() == Movie("Moana", 2016) #with_first_movie
+        watchlist.remove_movie(Movie("Moana", 2016))
+        assert watchlist.first_movie_in_watchlist() is None  #with_no_first_movie
+
+    def test_size(self):
+        watchlist.add_movie(Movie("Moana", 2016))
+        watchlist.add_movie(Movie("Ice Age", 2002))
+        watchlist.add_movie(Movie("Guardians of the Galaxy", 2012))
+        assert watchlist.size() == 3
+
+    def test_add_existing_movie_in_watchlist(self):
+        watchlist.add_movie(Movie("Moana", 2016)) #add_new_movie_in_watchlist
+        assert watchlist.size() == 1
+        watchlist.add_movie(Movie("Moana", 2016)) #add_existing_movie_in_watchlist
+        assert watchlist.size() == 1
+
+    def test_remove_movie_not_in_watchlist(self):
+        watchlist.add_movie(Movie("Moana", 2016))
+        assert watchlist.size() == 1
+        watchlist.remove_movie(Movie("Parasite", 2019))
+        assert watchlist.size() == 1
+
+    def test_select_movie_to_watch(self):
+        watchlist.add_movie(Movie("Moana", 2016))
+        assert watchlist.first_movie_in_watchlist() == Movie("Moana", 2016)
+        assert watchlist.select_movie_to_watch(0) == Movie("Moana", 2016) #when movie in watchlist
+        assert watchlist.select_movie_to_watch(watchlist.size() + 2) == None #when movie not in watchlist
 
 
 
